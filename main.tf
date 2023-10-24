c386c8a9465b97fdc4a9edc8fcb3c03e9913c62e
munnep <patrick.munne@hashicorp.com>
2023-10-24 12:18:38 +0200

    code

---
+++ main.tf
@@ -0,0 +1,15 @@
+variable "cloudflare_api_token" {}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.9"
+    }
+  }
+  required_version = ">= 0.14"
+}
